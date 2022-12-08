2eb480a3f24a976a3e2610ef431a0a773daf9230
ynawhocodes <ynawhocodes@gmail.com>
2022-12-08 12:56:22 +0900

    [feat] #3 user API 구현

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,82 @@
 const userModel = require("../models/authModel");
 
+// 회원가입
+async function createUser(req, res) {
+    var post = req.body;
+    var userInfo = [post.userSignId, post.userNickname, post.userPassword];
+    var result = await userModel.insertUser(userInfo);
+    if (!result)
+        res.json({ "result": "FAIL" });
+    else
+        res.json({ "result": "SUCCESS" });
+}
+
+// 로그인
+async function checkUserById(req, res) {
+    var post = req.body;
+    var result = await userModel.getUserById(post.userSignId);
+    if (!result.length) // 아이디 여부 확인
+        res.json({ "result": "FAIL" });
+    else {
+        if (result[0].user_password == post.password) // 비밀번호 확인
+            res.json({ "result": "SUCCESS", "userId": result[0].signId, "userNickname": result[0].nickname });
+        else
+            res.json({ "result": "FAIL" });
+    }
+
+}
+
+// 회원 정보 조회
+async function getUserInfoById(req, res) {
+    var post = req.body;
+    var result = await userModel.getUserById(post.userSignId);
+    if (!result.length) // 아이디 여부 확인
+        res.json({ "result": "FAIL" });
+    else
+        res.json({ "result": "SUCCESS", "userId": result[0].signId, "userNickname": result[0].nickname, "userStatusMsg": result[0].statusMsg });
+}
+
+// 회원 정보 수정
+async function updateUserInfo(req, res) {
+    var post = req.body;
+    var newUserInfo = [post.userNickname, post.userPassword, userStatusMsg, post.userSignId];
+    var result = await userModel.updateUserInfoById(newUserInfo);
+    if (!result)
+        res.json({ "result": "FAIL" });
+    else
+        res.json({ "result": "SUCCESS" });
+}
+
+// 회원 탈퇴 시, 비밀번호 확인
+async function getUserPasswordCorrect(req) {
+    var post = req.body;
+    var result = await userModel.getUserById(post.userSignId);
+    if (result[0].password == post.userPassword)
+        return true;
+    else
+        return false;
+}
+
+// 회원 탈퇴
+async function deleteUser(req, res) {
+    var post = req.body;
+    var correctResult = await getUserPasswordCorrect(req);
+    if (!correctResult) {
+        res.json({ "result": "FAIL" });
+    } else {
+        var result = await userModel.deleteUserById(correctResult);
+        if (!result)
+            res.json({ "result": "FAIL" });
+        else
+            res.json({ "result": "SUCCESS" });
+    }
+}
+
 module.exports = {
-    
+    createUser,
+    checkUserById,
+    getUserInfoById,
+    updateUserInfo,
+    getUserPasswordCorrect,
+    deleteUser
 }
--- models/authModel.js
+++ models/authModel.js
@@ -1,5 +1,61 @@
 const pool = require('../modules/pool');
 
+// 회원가입
+async function insertUser(user) {
+    const query = `INSERT INTO user (signId, nickname, password) VALUES (?, ?, ?)`;
+    try {
+        const result = await pool.queryParam(query, user);
+        return result;
+    } catch (error) {
+        return null;
+    }
+}
+
+// 아이디 여부 검사
+async function getUserById(userId) {
+    const query = `SELECT * FROM user WHERE signId=?`;
+    try {
+        const result = await pool.queryParam(query, userId).catch(
+            function(error) {
+                return null;
+            });
+        return result;
+    } catch (error) {
+        return null;
+    }
+}
+
+// 회원 정보 수정
+async function updateUserInfoById(newUserInfo) {
+    const query = `UPDATE user SET nickname=?, password=?, statusMsg=? WHERE signId=?`;
+    try {
+        const result = await pool.queryParam(query, newUserInfo).catch(
+            function(error) {
+                return null;
+            });
+        return result;
+    } catch (error) {
+        return null;
+    }
+}
+
+// 회원 탈퇴
+async function deleteUserById(userId) {
+    const query = `DELETE FROM user WHERE signId=?`;
+    try {
+        const result = await pool.queryParam(query, userId).catch(
+            function(error) {
+                return null;
+            });
+        return result;
+    } catch (error) {
+        return null;
+    }
+}
+
 module.exports = {
-    
+    insertUser,
+    getUserById,
+    updateUserInfoById,
+    deleteUserById
 }
