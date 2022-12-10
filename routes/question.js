1f90ca6889c67f94d5e713781d4602a8125c6628
yanwhocodes <ynawhocodes@gmail.com>
2022-12-10 12:35:22 +0900

    [feat] #5 question controller, model, route 구현

--- routes/question.js
+++ routes/question.js
@@ -2,4 +2,22 @@ var express = require('express');
 var router = express.Router();
 var questionController = require('../controllers/questionController');
 
+// 공개 질문 목록 조회
+router.get('/questions', questionController.getPublicQuestionListByGroupId);
+
+// 질문 조회
+router.get('/questions/:questionId', questionController.getQuestionByQuestionId);
+
+// 전체 그룹 질문 조회
+router.post('/questions/group', questionController.getAllGroupQuestionListByUserId);
+
+// 하나의 그룹에서 질문 목록 조회
+router.get('/questions/group/:groupId', questionController.getGroupQuestionListByGroupId);
+
+// 오늘의 질문 조회
+router.post('/questions/today', questionController.getDailyQuestion);
+
+// HOT 질문 조회
+router.post('/questions/hot', questionController.getDailyHotQuestion);
+
 module.exports = router;
